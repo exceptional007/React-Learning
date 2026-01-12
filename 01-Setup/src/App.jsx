7437ebd31f883a7645ad2407c2ffbd579eb4340e
exceptional007 <akshhatsri7843@gmail.com>
2026-01-12 23:58:13 +0530

    Setup Done

---
+++ 01-Setup/src/App.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+
+const App = () => {
+  return (
+    <>
+      <div id="parent">
+        <h1 id="child1"> Parent Child 1 </h1>
+        <h2 id="child2"> Parent Child 2 </h2>
+      </div>
+      <div id='chacha'>
+        <h1 id="child1">Chacha Child 1</h1>
+        <h2 id="child2">Chacha Child 2</h2>
+      </div>
+    </>
+  )
+}
+
+export default App
